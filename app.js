0528e4d0e890750b0198884dda0e7c3aaa05d632
Hammad-official-2520 <106146562+Hammad-official-2520@users.noreply.github.com>
2022-06-01 00:51:50 +0500

    assignment-2-js
    assignment-2-js

---
+++ app.js
@@ -0,0 +1,188 @@
+alert("i am 14 year old")
+alert("you have visited this site 14 times"  )
+var sec_variable = "Variable names can only contain ______, ______,______ and ______.For example $my_1stVariable "
+var thd_variable = "Variables must begin with a ______, ______ or_____. For example $name, _name or name"
+var frt_variable = "d) Variable names are case _________"
+var fit_variable = "Variable names should not be JS _________"
+document.write(sec_variable, "<br>",thd_variable, "<br>",frt_variable,"<br>", fit_variable ,"<br>");
+var first_num = +prompt ("Enter a first number")
+var sec_num = +prompt ("Enter a second number")
+var res = first_num + sec_num
+document.write("Sum of " , first_num ,  " and "   , sec_num , " is "  , res );
+
+
+ //----------------------------------------------------------question 02 -----------------------------// 
+  
+
+ alert(topic) 
+ var topic = "5 legal variable name\nalert\nvar\nif\nconsole\ndocument" 
+  
+ //----------------------------------------------------------question 03---------------------------//4 
+ document.write("<h1>rules for naming js variable </h1> </br> <p> variable name can only contain, number, names  </p> </br> <p> variable must begin with a name , small letter  </p> </br> <p> variable name are case sensitive  </p> </br> <p> variable name should not be js keywords  </p>   " )  
+  
+ // chap 05 
+
+ //----------------------------------------------------------question 01 -----------------------------// and 02 
+ 
+ var a = 4 
+ var b = 8 
+ document.write("sum of 4 and 8 is " ) 
+ document.write( a+b ) 
+ var a = 4 
+ var b = 8 
+ document.write("subtraction of 4 and 8 is " ) 
+ document.write( a-b ) 
+ var a = 4 
+ var b = 8 
+ document.write("product of 4 and 8 is " ) 
+ document.write( a*b ) 
+ var a = 4 
+ var b = 8 
+ document.write("division of 4 and 8 is " ) 
+ document.write( a/b ) 
+  
+ //----------------------------------------------------------question 03 -----------------------------// 
+  
+ var a = 2  
+ document.write(a ) 
+ document.write("</br>") 
+ document.write("value after increament is ") 
+  
+ document.write( ++a) 
+  
+ document.write("</br> value after adding 6 is ") 
+ document.write ( a+6) 
+ document.write("</br> value after decrement is ") 
+ document.write( --a ) 
+ document.write("</br> Reminder after dividing by 2 is   ") 
+ document.write( "0") 
+  
+//----------------------------------------------------------question 04 ---------------------------//
+  
+ var ticket = 600 
+ document.write("total coast to buy 5 movie is ") 
+ document.write( ticket*5) 
+ document.write("PKR") 
+  
+ //----------------------------------------------------------question 05 ---------------------------//
+ var a = 4 
+ document.write("<h1> Table of 4</h1>") 
+ document.write("</br><h4>4 x = 1</h4> ") 
+ document.write(a*1) 
+ document.write("</br><h4>4 x 2 = </h4> ") 
+ document.write(a*2) 
+ document.write("</br><h4>4 x 3 = </h4> ") 
+ document.write(a*3) 
+ document.write("</br><h4>4 x 4 = </h4> ") 
+ document.write(a*4) 
+ document.write("</br><h4>4 x 5 = </h4> ") 
+ document.write(a*5) 
+ document.write("</br><h4>4 x 6 = </h4> ") 
+ document.write(a*6) 
+ document.write("</br><h4>4 x 7 = </h4> ") 
+ document.write(a*7) 
+ document.write("</br><h4>4 x 8 = </h4> ") 
+ document.write(a*8) 
+ document.write("</br><h4>4 x 9 = </h4> ") 
+ document.write(a*9) 
+ document.write("</br><h4>4 x 10 = </h4> ") 
+ document.write(a*10 ,"<br>","<br>") 
+  
+ //----------------------------------------------------------question 06 -----------------------------//  
+
+var celcius = +prompt ("Enter value in °C")
+var fran =  (celcius * 9/5 + 32)
+
+document.write ( "<br>",celcius ," °C is equal to " , fran," °F " ,"<br>", "<br>" )
+  
+
+  
+ //----------------------------------------------------------question 07 ---------------------------//
+ var item1 = 500 
+ var item2 = 300 
+ document.write( "<br>","price of juice is " ) 
+ document.write(item1) 
+  
+ document.write("</br>") 
+ document.write("quantity of juice is 2") 
+ document.write("</br>") 
+  
+  
+  
+ document.write("price of biscuit is  ") 
+ document.write(item2) 
+  
+ document.write("</br>") 
+ document.write("quantity of biscuit is 8") 
+ document.write("</br>") 
+ document.write("total cost of your order is") 
+  
+ document.write(item1*2 + item2*8 ) 
+  
+  
+ //----------------------------------------------------------question 08 ---------------------------//
+  
+  
+ var obtain = 515 
+ var total = 550 
+  
+ document.write("<h1>Marks sheet </h1>") 
+ document.write("</br>") 
+ document.write("</br>") 
+ document.write("</br>") 
+  
+ document.write("Total Marks: ") 
+ document.write(total) 
+ document.write("</br>") 
+ document.write("Mraks obtained: ") 
+ document.write(obtain) 
+ document.write("</br>") 
+ document.write("Percentage: ") 
+  
+  
+ document.write((obtain/total)*100 ) 
+  
+ //----------------------------------------------------------question 09 ---------------------------//
+  
+  
+ var riyal=53.44 
+ var usd=200.45 
+ document.write("Total currency in PKR= ") 
+ document.write(10*usd + 25*riyal) 
+  
+ //----------------------------------------------------------question 10 ---------------------------//
+  
+ var a=6 
+ alert(((a+5)*10)/2) 
+  
+ //----------------------------------------------------------question 11 ---------------------------//
+  
+ var currentyear=2022 
+ var birthyear=2002 
+  
+ document.write("Current year: ") 
+ document.write(currentyear) 
+ document.write("</br> ") 
+ document.write("birth year: ") 
+ document.write(birthyear) 
+ document.write("</br>") 
+ document.write("your age: ") 
+ document.write(currentyear-birthyear) 
+  
+  
+ //----------------------------------------------------------question 12 -----------------------------// 
+  
+ var r=25 
+ document.write("<h1>THE GEOMETRIZER </h1>") 
+ document.write("</br>") 
+ document.write("radius of a circle is ") 
+ document.write(25) 
+ document.write("</br>") 
+ document.write("The circumference is ") 
+ document.write(2*3.142*r) 
+ document.write("</br>") 
+ document.write("The area is ") 
+ document.write(3.142*r*r)
+
+
+
